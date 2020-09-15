3953ded7406d15f9dd8cb7619ecf86652168563b
VigneshBharaniKumar <vignesh.b.qa@gmail.com>
2020-09-15 07:10:47 +0530

    Log code Modified

--- app/src/main/java/com/vignesh/gitandroidapp/MainActivity.java
+++ app/src/main/java/com/vignesh/gitandroidapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("TAG", "Message");
+        Log.i("TAG", "Message Added");
 
     }
 }
